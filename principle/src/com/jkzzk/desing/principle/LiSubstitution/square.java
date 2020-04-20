bedb91868db32b22b76c65ab166122edc8355ba5
jkzzk <1745493589@qq.com>
2020-04-20 18:17:31 +0800

    设计模式-设计原则

---
+++ principle/src/com/jkzzk/desing/principle/LiSubstitution/square.java
@@ -0,0 +1,16 @@
+package com.jkzzk.desing.principle.LiSubstitution;
+
+/**
+ *  正方形
+ */
+public class square {
+    private long sideLength;
+
+    public long getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(long sideLength) {
+        this.sideLength = sideLength;
+    }
+}
